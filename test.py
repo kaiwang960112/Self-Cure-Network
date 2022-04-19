cc2cda32327b4efff3deb9c1114ff207a2f9e587
Kai Wang (Victor Kai) <37533040+kaiwang960112@users.noreply.github.com>
2022-04-19 18:03:01 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
+####### This code was created by @bruceche11 in July 2020, some people tell me this code has some problems in load model and so on. 
+####### Therefore, I decide to delet this file. 19th April 2022.
+
+
 import cv2
 import torch
 from torchvision import transforms
